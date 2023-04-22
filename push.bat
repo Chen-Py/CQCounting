2af51ca3060382c306406821dbe3bed9d73bbc48
Chen-Py <chenpyer@qq.com>
2023-04-22 12:06:12 +0800

    2023/04/22 周六 12:06:12.37

--- push.bat
+++ push.bat
@@ -1,15 +1,14 @@
-rm ./*.toc
-rm ./*.gz
-rm ./*.xml
-rm ./*.out
-rm ./*.nav
-rm ./*.log
-rm ./*.bcf
-rm ./*.aux
-rm ./*.snm
-mv -Force ./main.pdf docs
+del *.toc
+del *.gz
+del *.xml
+del *.out
+del *.nav
+del *.log
+del *.bcf
+del *.aux
+del *.snm
+move -Force main.pdf docs
 git add *
 set now=%date% %time%
-echo "Time:" %now%
 git commit -m "%now%"
 git push
