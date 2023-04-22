f9bc67585eb46ba77fcc94ac8e0d96aa4b6de880
Chen-Py <chenpyer@qq.com>
2023-04-22 12:02:44 +0800

    2023/04/22 周六 12:02:44.31

--- push.bat
+++ push.bat
@@ -1,13 +1,13 @@
-rm *.toc
-rm *.gz
-rm *.xml
-rm *.out
-rm *.nav
-rm *.log
-rm *.bcf
-rm *.aux
-rm *.snm
-mv -Force .\main.pdf docs
+rm ./*.toc
+rm ./*.gz
+rm ./*.xml
+rm ./*.out
+rm ./*.nav
+rm ./*.log
+rm ./*.bcf
+rm ./*.aux
+rm ./*.snm
+mv -Force ./main.pdf docs
 git add *
 set now=%date% %time%
 echo "Time:" %now%
